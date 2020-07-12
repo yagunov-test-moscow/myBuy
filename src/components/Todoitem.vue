69fa493e78fc608ef43a75affff465dd924f1581
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2020-07-12 17:01:36 +0300

    create card

--- src/components/Todoitem.vue
+++ src/components/Todoitem.vue
@@ -1,11 +1,15 @@
 <template>
  <div class="row">
-      <div class="card-panel teal">
-        <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-        I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-        </span>
-      </div>
+  <div class="card-panel teal">
+   <label>
+    <input type="checkbox"/>
+     <span style="font-size: 1.9rem; color: white;" class="check"></span>
+    </label>
+    <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+    </span>
+    <a class="waves-effect waves-teal btn-flat"><i class="material-icons" style="font-size: 1.9rem; color: white;">delete_forever</i></a>
   </div>
+ </div>
 </template>
 
 <script>
@@ -15,5 +19,22 @@ export default {
 </script>
 
 <style scoped>
+.card-panel {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+}
+.btn {
+  margin-left: 5px;
+  overflow: inherit;
+}
+span.check::before{
+  border-color: white;
+}
+[type="checkbox"]:checked+span:not(.lever):before{
+    border-right: 2px solid white;
+    border-bottom: 2px solid white;
+}
 
 </style>
